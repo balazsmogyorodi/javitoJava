151d605d07f510e3e2a5d4c7abb3de87ca365a92
Mogyoródi Balázs <mogyorodi.balazs@dk.akkszalezi.hu>
2024-01-15 15:29:07 +0100

    lámpa váltások

--- src/bicikli/Lampa.java
+++ src/bicikli/Lampa.java
@@ -3,9 +3,10 @@ package bicikli;
 import bicikli.Main.LampaAllapotok;
 
 public class Lampa {
+    
 
     private LampaAllapotok lampa;
-    private int allapot;
+    private int allapot, maximum_beallitas = 5;
 
     public LampaAllapotok getLampa() {
         return lampa;
@@ -17,8 +18,33 @@ public class Lampa {
 
     public void allapotValtas() {
         allapot += 1;
-     
+        if (allapot > maximum_beallitas) {
+            allapot = 0;
+        }
+        fokozatok();
+    }
 
+    private void fokozatok() {
+        switch (allapot) {
+            case 0:
+                setLampa(lampa.Sötét);
+                break;
+            case 1:
+                setLampa(lampa.Fényes);
+                break;
+            case 2:
+                setLampa(lampa.Erősebb);
+                break;
+            case 3:
+                setLampa(lampa.Vilogó);
+                break;
+            case 4:
+                setLampa(lampa.Erősebb_Vilogó);
+                break;
+            case 5:
+                setLampa(lampa.Szin_jelző_fény);
+                break;
+        }
     }
 
     public Lampa() {
--- src/bicikli/Main.java
+++ src/bicikli/Main.java
@@ -3,24 +3,32 @@ package bicikli;
 import bicikli.Main.LampaAllapotok;
 
 public class Main {
-    
+
     enum LampaAllapotok {
-        Sötét, Fény, Erősebb, Vilogó, Erősebb_Vilogó, Szin_jelző_fény
+        Sötét, Fényes, Erősebb, Vilogó, Erősebb_Vilogó, Szin_jelző_fény
     };
 
     public static void main(String[] args) {
         Lampa l = new Lampa();
         kiiras(l);
-        
+        l.allapotValtas();
+        kiiras(l);
+        l.allapotValtas();
+        kiiras(l);
+        l.allapotValtas();
+        kiiras(l);
+        l.allapotValtas();
+        kiiras(l);
+        l.allapotValtas();
+        kiiras(l);
+        l.allapotValtas();
+        kiiras(l);
+
     }
-    
-    
-    
-    
-    
+
     public static void kiiras(Lampa lampa) {
         System.out.println("A bicikli lámpájának az állapota jelenleg: " + lampa.getLampa());
-        
+
     }
-    
+
 }
